9d7963084da964e1d727bafa3c97d0261a0b492f
Adrean Torres <126034639+Mrnooqmal@users.noreply.github.com>
2025-09-08 16:39:47 -0300

    12/12

--- miApp/src/app/dc/dc.page.ts
+++ miApp/src/app/dc/dc.page.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonItem} from '@ionic/angular/standalone';
-
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -9,18 +9,23 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonIt
   templateUrl: './dc.page.html',
   styleUrls: ['./dc.page.scss'],
   standalone:true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonButton, IonLabel, IonItem],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonButton, IonLabel, IonItem, RouterModule],
 })
 
 export class DcPage {
     description = 'DC Comics es una de las principales editoriales de cómics en el mundo, conocida por sus icónicos superhéroes como Superman,Batman,Flash, etc... Fundada en 1934, el padre de los Comics de superherores';
     foto = 'assets/companias_comics/dc.jpg';
+
+    mostrarImagen = '';  
+    mostrarTexto = '';
     constructor() { }
     MostrarDescripcionDc() {
-        return this.description;
+        this.mostrarTexto= this.description;
+        return this.mostrarTexto
     }
     MostrarFotoDc() {
-        return this.foto;
+        this.mostrarImagen= this.foto;
+        return this.mostrarImagen
     }   
     ngOnInit() {
     }
--- miApp/src/app/marvel/marvel.page.ts
+++ miApp/src/app/marvel/marvel.page.ts
@@ -1,25 +1,31 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonItem} from '@ionic/angular/standalone';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-marvel',
   templateUrl: './marvel.page.html',
   styleUrls: ['./marvel.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonButton, IonLabel, IonItem],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonButton, IonLabel, IonItem, RouterModule],
   standalone: true
 })
 export class MarvelPage {
 
     description = 'Marvel es una compañía de entretenimiento conocida por sus cómics, películas y series... Nació en 1939 y es la hija de DC Comics';
     foto = 'assets/companias_comics/marvel.jpg';
+
+    mostrarTexto = ''; 
+    mostrarImagen = '';  
+  
     constructor() { }
     MostrarDescripcionMarvel() {
-        return this.description;
+        this.mostrarTexto= this.description;
+        return this.mostrarTexto
     }
     MostrarFotoMarvel() {
-        return this.foto;
+        this.mostrarImagen= this.foto;
+        return this.mostrarImagen
     }
     ngOnInit() {
     }
